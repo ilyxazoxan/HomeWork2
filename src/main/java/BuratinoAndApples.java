a80a418e607a11092104b90b05f238bd3cd785b9
Ilya Danilov <esozap32@mail.ru>
2022-10-28 23:51:30 +0400

    BuratinoAndApples

---
+++ src/main/java/BuratinoAndApples.java
@@ -0,0 +1,10 @@
+public class BuratinoAndApples {
+    public static void main(String[] args) {
+
+    int buratinosApples = 10;
+    int karabasBarabasStole = 2;
+    int remained = buratinosApples - karabasBarabasStole;
+     System.out.println ("У буратино осталось яблок: " + remained);
+}
+
+}
