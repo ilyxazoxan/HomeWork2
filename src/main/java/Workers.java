b40ed1c91352e48caadc9229a65c49b9485102e7
Ilya Danilov <esozap32@mail.ru>
2022-10-29 21:54:12 +0400

    Workers

---
+++ src/main/java/Workers.java
@@ -0,0 +1,12 @@
+public class Workers {
+    public static void main(String[]args) {
+        int programmer = 4;
+        int tester = 2;
+        int support = 3;
+        int alltesters = tester * programmer;
+        int allsupport = support * programmer;
+        int allworkers = programmer + alltesters + allsupport;
+System.out.print ("Всего людей на проекте: " + allworkers);
+
+    }
+}
