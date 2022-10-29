a45bc8f6a7d31ba850f13768ae03cf045a0e1ebe
Ilya Danilov <esozap32@mail.ru>
2022-10-29 23:12:10 +0400

    Mortgage

---
+++ src/main/java/Mortgage.java
@@ -0,0 +1,9 @@
+public class Mortgage {
+    public static void main(String[] args) {
+        long TheCostOfTheApartment = 10500000L;
+        double initialPayment = 0.22;
+        double theAmount = TheCostOfTheApartment * initialPayment;
+        System.out.println("Необходимая сумма для первоначального взноса " + theAmount + " Рублей");
+
+    }
+}
